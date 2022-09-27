7391cb392bc2848a04d3a5cbb512f2742ada37e3
Gabe <66648962+gabriel-ross@users.noreply.github.com>
2022-09-27 16:35:28 -0400

    remove unused code from main.go

--- cmd/main.go
+++ cmd/main.go
@@ -12,18 +12,4 @@ func main() {
 	s := server.NewServer()
 	fmt.Printf("Running server on port %s\n", PORT)
 	http.ListenAndServe(":"+PORT, s)
-	// resp, err := http.Get("https://api.hatchways.io/assessment/blog/posts?tag=tech")
-	// if err != nil {
-	// 	log.Fatal("error: ", err)
-	// }
-	// defer resp.Body.Close()
-	// data, err := io.ReadAll(resp.Body)
-	// fmt.Printf("raw data: %s\n", data)
-	// if err != nil {
-	// 	log.Fatal("error2: ", err)
-	// }
-	// respArr := server.APIResponse{}
-	// err = json.Unmarshal(data, &respArr)
-	// fmt.Printf("%v", respArr)
-	// fmt.Printf("%s", reflect.TypeOf(respArr.Posts[0]))
 }
